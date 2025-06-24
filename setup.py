8734f17d30971bd19cf9b10695e98fb99066db72
Никто Никто <167694532+SeniorShadifer@users.noreply.github.com>
2025-06-24 17:56:22 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     package_data={"client": ["*.html", "css/*.css", "css/*/*.css", "js/*.js"]},
     install_requires=[
         "cryptography==44.0.2",
+        "platformdirs==4.3.8",
+        "setuptools==80.8.0",
         "termcolor==3.1.0",
         "requests==2.32.3",
         "pywebview==5.4",
         "flask==3.1.1",
-        "platformdirs==4.3.8",
     ],
     entry_points={
         "console_scripts": [
